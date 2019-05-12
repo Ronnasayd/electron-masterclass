d06004268028a1a75382c3a7b3b38d6486d873c0
Ronnasayd Machado <ronnasayd@hotmail.com>
2019-05-12 11:08:37 -0300

    first commit

---
+++ mainWindow.js
@@ -0,0 +1,28 @@
+// Modules
+const { BrowserWindow } = require('electron')
+
+// browserWindow instance
+exports.win
+
+//mainWindow createWindow fn
+exports.createWindow = () => {
+    this.win = new BrowserWindow({
+        width: 500,
+        height: 650,
+        minWidth: 350,
+        maxWidth: 650,
+        minHeight: 310,
+
+    })
+
+    // devTools
+    this.win.webContents.openDevTools()
+
+    //Load mainWindow content
+    this.win.loadURL(`file://${__dirname}/renderer/main.html`)
+
+    //handle window close
+    this.win.on('close', () => {
+        this.win = null
+    })
+}
