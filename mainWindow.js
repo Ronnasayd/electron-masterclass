0f8a5bc4321844ce5907c739da71b2633773fab2
Ronnasayd Machado <ronnasayd@hotmail.com>
2019-05-13 17:59:51 -0300

    finish

--- mainWindow.js
+++ mainWindow.js
@@ -1,33 +1,33 @@
+
 // Modules
 const { BrowserWindow } = require('electron')
 
-// browserWindow instance
+// BrowserWindow instance
 exports.win
 
-//mainWindow createWindow fn
+// mainWindow createWindow fn
 exports.createWindow = () => {
-    this.win = new BrowserWindow({
-        width: 500,
-        height: 650,
-        minWidth: 350,
-        maxWidth: 650,
-        minHeight: 310,
 
-        webPreferences: {
-            nodeIntegration: true,
-            webviewTag: true
-        }
+  this.win = new BrowserWindow({
+    width: 500,
+    height: 650,
+    minWidth: 350,
+    maxWidth: 650,
+    minHeight: 310,
+    icon: `${__dirname}/icons/64x64.png`,
+    webPreferences: {
+      nodeIntegration: true,
+      webviewTag: true
+    }
+  })
 
-    })
 
-    // devTools
-    this.win.webContents.openDevTools()
 
-    //Load mainWindow content
-    this.win.loadURL(`file://${__dirname}/renderer/main.html`)
+  // Load main window content
+  this.win.loadURL(`file://${__dirname}/renderer/main.html`)
 
-    //handle window close
-    this.win.on('close', () => {
-        this.win = null
-    })
+  // Handle window closed
+  this.win.on('closed', () => {
+    this.win = null
+  })
 }
--- renderer/app.js
+++ renderer/app.js
@@ -1,79 +1,89 @@
-//Modules
-const { ipcRenderer } = require('electron')
-const items = require('./items')
 
-// Navigate item with up/down keys
+// Modules
+const {ipcRenderer} = require('electron')
+const items = require('./items.js')
+const menu = require('./menu.js')
+
+// Navigate selected item with up/down keys
 $(document).keydown((e) => {
-    switch (e.key) {
-        case 'ArrowUp':
-            items.changeItem('up')
-            break;
-        case 'ArrowDown':
-            items.changeItem('down')
-            break;
-    }
+
+  switch (e.key) {
+    case 'ArrowUp':
+      items.changeItem('up')
+      break;
+    case 'ArrowDown':
+      items.changeItem('down')
+      break;
+  }
 })
 
-// Show add modal
+// Show add-modal
 $('.open-add-modal').click(() => {
-    $('#add-modal').addClass('is-active');
-});
-
-// hidden add modal
+  $('#add-modal').addClass('is-active')
+})
+// Hide add-modal
 $('.close-add-modal').click(() => {
-    $('#add-modal').removeClass('is-active');
-});
-
-//handle add-modal submission
-$('#add-button').click(() => {
-    // Get url from input
-    let newItemURL = $('#item-input').val();
-    if (newItemURL) {
-        $('#item-input').prop('disabled', true)
-        $('#add-button').addClass('is-loading')
-        $('.close-add-modal').addClass('is-disabled')
-
-        ipcRenderer.send('new-item', newItemURL)
-    }
-});
-
-$('#item-input').keyup((e) => {
-    if (e.key === 'Enter') {
-        $('#add-button').click();
-    }
+  $('#add-modal').removeClass('is-active')
 })
 
+// Handle add-modal submission
+$('#add-button').click(() => {
+
+  // Get URL from input
+  let newItemURL = $('#item-input').val()
+  if(newItemURL) {
+
+    // Disable modal UI
+    $('#item-input').prop('disabled', true)
+    $('#add-button').addClass('is-loading')
+    $('.close-add-modal').addClass('is-disabled')
+
+    // Send URL to main process via IPC
+    ipcRenderer.send('new-item', newItemURL)
+  }
+})
+
+// Listen for new item from main
 ipcRenderer.on('new-item-success', (e, item) => {
 
-    //Add Item to items array
-    items.toreadItems.push(item)
-    // Save items
-    items.saveItems()
-    // Add Item
-    items.addItem(item)
+  // Add item to items array
+  items.toreadItems.push(item)
 
-    // Close and reset modal
-    $('#add-modal').removeClass('is-active')
-    $('#item-input').prop('disabled', false).val('')
-    $('#add-button').removeClass('is-loading')
-    $('.close-add-modal').removeClass('is-disabled')
+  // Save items
+  items.saveItems()
 
-    // If fist item  being added select it
-    if (items.toreadItems.length === 1)
-        $('.read-item:first()').addClass('is-active')
-})
+  // Add item
+  items.addItem(item)
 
-$('#search').keyup((e) => {
-    // Get current search input
-    let filter = $(e.target).val()
+  // Close and reset modal
+  $('#add-modal').removeClass('is-active')
+  $('#item-input').prop('disabled', false).val('')
+  $('#add-button').removeClass('is-loading')
+  $('.close-add-modal').removeClass('is-disabled')
 
-    $('.read-item').each((i, el) => {
-        $(el).text().toLowerCase().includes(filter) ? $(el).show() : $(el).hide()
-    })
-})
-
-// Add items when app load
-if (items.toreadItems.length) {
-    items.toreadItems.forEach(items.addItem)
+  // If first item being added, select it
+  if(items.toreadItems.length === 1)
     $('.read-item:first()').addClass('is-active')
+})
+
+// Simulate add click on enter
+$('#item-input').keyup((e) => {
+  if(e.key === 'Enter') $('#add-button').click()
+})
+
+// Filter items by title
+$('#search').keyup((e) => {
+
+  // Get current #search input value
+  let filter = $(e.currentTarget).val()
+
+  $('.read-item').each((i, el) => {
+    $(el).text().toLowerCase().includes(filter) ? $(el).show(): $(el).hide()
+  })
+})
+
+// Add items when app loads
+if(items.toreadItems.length) {
+  items.toreadItems.forEach(items.addItem)
+  $('.read-item:first()').addClass('is-active')
 }
